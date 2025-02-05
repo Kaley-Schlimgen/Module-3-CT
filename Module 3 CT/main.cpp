b0c96f5eee69222f3209343e872cf23d31ae76f0
Kaley-Schlimgen <kaleynicole89@gmail.com>
2025-02-04 17:37:52 -0700

    commit

--- Module 3 CT/main.cpp
+++ Module 3 CT/main.cpp
@@ -4,19 +4,26 @@
 //
 //  Created by Kaley Schlimgen on 1/30/25.
 //
+// Program:
+// Create a C++ program that asks the user to enter three integer values as input.
+// Store the values into three different variables.
+// For each vairable, create an integer pointer to dynamic memory.
+// Display the contents of the vairables and pointers.
+// Be sure to use the new operator and delete operators to manage memory.
+//
 
 #include <iostream>
-using namespace std;
+//using namespace std;
 
 int main(int argc, const char * argv[]) {
     
     int num1, num2, num3;
     
-    cout<<"Enter three integers: "<<endl;
-    cin>>num1>>num2>>num3;
+    std::cout<<"Enter three integers: "<<std::endl;
+    std::cin>>num1>>num2>>num3;
     
     
-    cout<<"Output is: "<<num1<< ", " <<num2<< " & " <<num3;
+    std::cout<<"You entered: "<<num1<< ", " <<num2<< " & " <<num3<<std::endl;
     
     return 0;
 }
