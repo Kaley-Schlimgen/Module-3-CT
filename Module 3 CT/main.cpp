ab92f475b024f625959efb7cf080476938e8c425
Kaley-Schlimgen <kaleynicole89@gmail.com>
2025-02-05 06:59:34 -0700

    All components added

--- Module 3 CT/main.cpp
+++ Module 3 CT/main.cpp
@@ -17,18 +17,24 @@
 
 int main(int argc, const char * argv[]) {
     int *ptr1;
+    int *ptr2;
+    int *ptr3;
     
     int num1, num2, num3;
     std::cout<<"Enter three integers: "<<std::endl;
     std::cin>>num1>>num2>>num3;
     
     ptr1 = new int(num1);
+    ptr2 = new int(num2);
+    ptr3 = new int(num3);
     
-    std::cout<<"Contents of Variables: "<<*ptr1<< ", " <<num2<< " & " <<num3<<std::endl;
+    std::cout<<"Contents of Variables: "<<num1<< ", " <<num2<< " & " <<num3<<std::endl;
     
-    std::cout<<"Contents of Pointers: "<<*ptr1<< ", " <<num2<< " & " <<num3<<std::endl;
+    std::cout<<"Contents of Pointers: "<<*ptr1<< ", " <<*ptr2<< " & " <<*ptr3<<std::endl;
     
     delete ptr1;
+    delete ptr2;
+    delete ptr3;
     
     return 0;
 }
