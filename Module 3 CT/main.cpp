022a6a8f1eee21068f6118636319678923a07e02
Kaley-Schlimgen <kaleynicole89@gmail.com>
2025-02-05 07:24:21 -0700

    Commit with comments

--- Module 3 CT/main.cpp
+++ Module 3 CT/main.cpp
@@ -1,37 +1,45 @@
-//
-//  main.cpp
-//  Module 3 CT
-//
-//  Created by Kaley Schlimgen on 1/30/25.
-//
-// Program:
-// Create a C++ program that asks the user to enter three integer values as input.
-// Store the values into three different variables.
-// For each vairable, create an integer pointer to dynamic memory.
-// Display the contents of the vairables and pointers.
-// Be sure to use the new operator and delete operators to manage memory.
-//
+/*
+ *  main.cpp
+ *  Module 3 CT
+ *
+ *  Created by Kaley Schlimgen on 1/30/25.
+ *
+ *  Program:
+ *  Create a C++ program that asks the user to enter three integer values as input.
+ *  Store the values into three different variables.
+ *  For each variable, create an integer pointer to dynamic memory.
+ *  Display the contents of the vairables and pointers.
+ *  Be sure to use the new operator and delete operators to manage memory.
+ */
 
 #include <iostream>
-//using namespace std;
 
 int main(int argc, const char * argv[]) {
-    int *ptr1;
-    int *ptr2;
-    int *ptr3;
+    //Create integer pointer to dynamic memory for three variables
+    //Each new operator in the lines below allocates enough memory for a single integer
+    int *ptr1 = new int;
+    int *ptr2 = new int;
+    int *ptr3 = new int;
     
+    //Declare three variables of integer type
     int num1, num2, num3;
-    std::cout<<"Enter three integers: "<<std::endl;
+    
+    //Program asks user to enter three integer values as input
+    std::cout<<"Enter Three Integers: "<<std::endl;
+    //The three input values are received and stored into three different variables
     std::cin>>num1>>num2>>num3;
     
-    ptr1 = new int(num1);
-    ptr2 = new int(num2);
-    ptr3 = new int(num3);
+    //Assign the pointers to the variables
+    *ptr1 = num1;
+    *ptr2 = num2;
+    *ptr3 = num3;
     
+    //Display contents of variables
     std::cout<<"Contents of Variables: "<<num1<< ", " <<num2<< " & " <<num3<<std::endl;
-    
+    //Display contents of pointers
     std::cout<<"Contents of Pointers: "<<*ptr1<< ", " <<*ptr2<< " & " <<*ptr3<<std::endl;
     
+    //delete operator deallocates pointers when no longer needed
     delete ptr1;
     delete ptr2;
     delete ptr3;
